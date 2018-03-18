bdf933bcbc231fc578d753cd8111acdec9f3216f
Izentar <37346098+Izentar@users.noreply.github.com>
2018-03-18 19:04:13 +0100

    Add files via upload
    Done without files

--- main.cpp
+++ main.cpp
@@ -2,26 +2,18 @@
     * About:  Students group creator
     * Author: Mateusz Kordowski
     * Leader: Aleksandra Paśnikowska
-    * Update: 11.03.2018
+    * Update: 18.03.2018
 */
-
-#define DEBUG
 
 #include <iostream>
 
 #include "headers/student.h"
 
-
 using namespace std;
 
 int main(int ac, char* av[], char* ev[])
 {
-    cout << "Hello world!" << endl;
-
-    S_group *A;
-    A=new S_group;
-
-
+    menu();
 
     return 0;
 }
